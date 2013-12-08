NodeBB/NodeBB

commit f1ad4698613d59bc2648b359accd6c6e196434cc

templates - don't clean up selector conditionals; fixes topic posting bug introduced at 090d35f306bd8d471ab347d8f524afdb1bc244c3

public/src/templates.js
@@ -320,16 +320,16 @@
 									if (conditionalBlock[1]) {
 										// there is an else statement
 										if (!value) {
-											template = template.replace(matches[i], conditionalBlock[1].replace(/<!-- ((\IF\b)|(\bENDIF\b))(.*?)-->/gi, ''));
+											template = template.replace(matches[i], conditionalBlock[1].replace(/<!-- ((\IF\b)|(\bENDIF\b))([^@]*?)-->/gi, ''));
 										} else {
-											template = template.replace(matches[i], conditionalBlock[0].replace(/<!-- ((\IF\b)|(\bENDIF\b))(.*?)-->/gi, ''));
+											template = template.replace(matches[i], conditionalBlock[0].replace(/<!-- ((\IF\b)|(\bENDIF\b))([^@]*?)-->/gi, ''));
 										}
 									} else {
 										// regular if statement
 										if (!value) {
 											template = template.replace(matches[i], '');
 										} else {
-											template = template.replace(matches[i], matches[i].replace(/<!-- ((\IF\b)|(\bENDIF\b))(.*?)-->/gi, ''));
+											template = template.replace(matches[i], matches[i].replace(/<!-- ((\IF\b)|(\bENDIF\b))([^@]*?)-->/gi, ''));
 										}
 									}
 								}

ITsolution-git/node-forum

commit 10fd4e5418cbb2166f977a7c68277dc6b867ef1f

fixes #636, conditionals broken from f1ad4698613d59bc2648b359accd6c6e196434cc

public/src/templates.js
@@ -355,7 +355,7 @@
 			}
 			
 			// clean up all undefined conditionals
-			template = template.replace(/<!-- IF([\s\S]*?)ENDIF([\s\S]*?)-->/gi, '');
+			template = template.replace(/<!-- IF([^@]*?)ENDIF([^@]*?)-->/gi, '');
 
 			return template;
 